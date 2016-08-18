1af3df93280d79c3b16ff25c2f08c46eb3706ee4
trankilloman <trankilloman@gmail.com>
2016-08-18 11:48:42 +0200

    fonctionnel

--- autofunchanger.sh
+++ autofunchanger.sh
@@ -1,23 +1,17 @@
 #! /bin/bash 
-# a travailler
-echo "recuperer nombre ligne"
-nvxhostname=`wc -l liste\ noms`
-echo "génération nombre aleatoire a regler en fonction de nvxhostname"
-nbrrdmhost=`echo $RANDOM %10+1 | bc`
-echo "affichage nombre aleatoire"
-echo $nbrrdmhost
-echo "mise en variable d'un 
-#sed -n '$nbrrdmhostp' liste\ noms 
 
 #fonctionnel
-
+echo "generation nvx hostname"
+nvxhostname=`cat liste\ noms | sort -R | head -n1`
+echo "affichage nvx hostname"
+echo $nvxhostname
 echo "récupération nom ancien hostname"
 ancienhostname=`cat /etc/hostname`
 echo "affichage ancien hostname"
 echo $ancienhostname
 echo "remplacement ancien hostname par nouveau dans fichier hostname"
 #remplacer hostname par /etc/hostname quand fonctionnel et changer testnouveau par variable avec nouveau nom
-sed -i "s|$ancienhostname|testnouveau|" hostname
+sed -i "s|$ancienhostname|$nvxhostname|" /etc/hostname
 echo "remplacement ancien hostname par nouveau dans fichier hosts"
 #remplacer hosts par /etc/hosts quand fonctionnel et changer testnouveau par variable avec nouveau nom
-sed -i "s|$ancienhostname|testnouveau|" hosts
+sed -i "s|$ancienhostname|$nvxhostname|" /etc/hosts
